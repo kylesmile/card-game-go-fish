963aeb935725b55ae523b6434316c4925c4aeffa
Kyle Smith <kyle@smithsrus.com>
2014-01-09 16:28:00 -0500

    Add sample generated data

--- go_fish_app.rb
+++ go_fish_app.rb
@@ -1,13 +1,29 @@
 require 'sinatra'
 require 'slim'
 
-hand = %w{sa ca da d2 h3 s7 cq hk}
-books = %w{8 j 10}
-opponents = [{name: 'Frodo B', cards: 3, books: 3},
-           {name: 'Meriadoc B', cards: 7, books: 2},
-           {name: 'Peregrin T', cards: 9, books: 1},
-           {name: 'Samwise G', cards: 1, books: 0}]
+opponents = [{name: 'Gandalf', cards: 3, books: 3},
+            {name: 'Radagast', cards: 7, books: 2},
+            {name: 'Galadriel', cards: 9, books: 1},
+            {name: 'Elrond', cards: 1, books: 0},
+            {name: 'Legolas', cards: 4, books: 1}]
+player_name = 'Sauron'
 
 get '/' do
-  slim :hand, locals: {hand: hand, books: books, opponents: opponents}
+  hand = []
+
+  7.times do
+    hand << %w{s c d h}.sample + %w{a 2 3 4 5 6 7 8 9 10 j q k}.sample
+  end
+
+  books = []
+
+  2.times do
+    books << %w{a 2 3 4 5 6 7 8 9 10 j q k}.sample
+  end
+  
+  slim :hand, locals: {hand: hand, books: books, opponents: opponents, player_name: player_name}
+end
+
+post '/' do
+  'You asked ' + params[:opponent] + ' for ' + params[:card]
 end
