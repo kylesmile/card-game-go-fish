cba2012fc44a11f59dfbcd3195700f3a2c5706ce
Kyle Smith <kyle@smithsrus.com>
2014-01-14 08:41:52 -0500

    Use GoFishGame object for data

--- go_fish_app.rb
+++ go_fish_app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'slim'
+require 'pry'
+require_relative './go_fish_game'
 
 class LoginScreen < Sinatra::Base
   enable :sessions
@@ -11,12 +13,20 @@ class LoginScreen < Sinatra::Base
       redirect '/login'
     else
       session['user_name'] = params[:name]
+      game = GoFishGame.new(3)
+      game.setup_game
+      session['game_id'] = game.object_id
+      GoFishApp.games[game.object_id] = game
       redirect '/'
     end
   end
 end
 
 class GoFishApp < Sinatra::Base
+  @@games = {}
+  def self.games
+    @@games
+  end
   # middleware will run before filters
   use LoginScreen
 
@@ -24,31 +34,32 @@ class GoFishApp < Sinatra::Base
     unless session['user_name']
       redirect '/login'
     end
+    @game = @@games[session['game_id']]
   end
 
   get '/' do
-    opponents = [{name: 'Gandalf', cards: 3, books: 3},
-            {name: 'Radagast', cards: 7, books: 2},
-            {name: 'Galadriel', cards: 9, books: 1},
-            {name: 'Elrond', cards: 1, books: 0},
-            {name: 'Legolas', cards: 4, books: 1}]
     
-    hand = []
+    @opponents = [{name: 'Gandalf', cards: 7, books: 0}, {name: 'Bob', cards: 7, books: 0}]
 
-    7.times do
-      hand << %w{s c d h}.sample + %w{a 2 3 4 5 6 7 8 9 10 j q k}.sample
-    end
-
-    books = []
+    @books = []
 
     2.times do
-      books << %w{a 2 3 4 5 6 7 8 9 10 j q k}.sample
+      @books << %w{a 2 3 4 5 6 7 8 9 10 j q k}.sample
     end
     
-    slim :hand, locals: {hand: hand, books: books, opponents: opponents, player_name: session['user_name']}
+    slim :hand
   end
 
-  post '/' do
-    'You asked ' + params[:opponent] + ' for ' + params[:card]
+  post '/turn' do
+    @result = @game.take_turn(params[:opponent].to_i, params[:card])
+    @opponents = [{name: 'Gandalf', cards: 7, books: 0}, {name: 'Bob', cards: 7, books: 0}]
+
+    @books = []
+
+    2.times do
+      @books << %w{a 2 3 4 5 6 7 8 9 10 j q k}.sample
+    end
+    
+    slim :hand
   end
 end
